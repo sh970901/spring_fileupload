7d97ca3108132141f1699c915dbdbaba81d74be9
sh970901 <aaddss639@naver.com>
2022-09-18 22:03:49 +0900

    setting

---
+++ src/main/java/com/ll/exam/profileapp/ProfileAppApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.profileapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileAppApplication.class, args);
+    }
+
+}
