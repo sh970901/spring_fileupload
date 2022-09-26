0575e1e4034cf53fc7e0587c57436e27e2057fdf
sh970901 <aaddss639@naver.com>
2022-09-26 13:41:12 +0900

    OAuth2UserService 등록

--- src/main/java/com/ll/exam/profileapp/app/security/SecurityConfig.java
+++ src/main/java/com/ll/exam/profileapp/app/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ll.exam.profileapp.app.security;
 
+import com.ll.exam.profileapp.app.member.service.OAuth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,22 +17,36 @@ import org.springframework.security.web.SecurityFilterChain;
 //이게 있어야 @PreAuthorize 가능
 //@Profile(value={"dev", "test"}) 운영용 개발용 가능
 public class SecurityConfig {
+    @Autowired
+    private OAuth2UserService oAuth2UserService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(
-                        csrf-> csrf.disable()
+                        csrf -> csrf.disable()
                 )
-                .authorizeRequests()
-                .antMatchers("/**")
-                .permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/member/login") //GET
-                .loginProcessingUrl("/member/login") //POST
-                .and()
-                .logout()
-                .logoutUrl("/member/logout");
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                .antMatchers("/**")
+                                .permitAll()
+                )
+                .formLogin(
+                        formLogin -> formLogin
+                                .loginPage("/member/login") // GET
+                                .loginProcessingUrl("/member/login") // POST
+                )
+                .oauth2Login(
+                        oauth2Login -> oauth2Login
+                                .loginPage("/member/login")
+                                .userInfoEndpoint(
+                                        userInfoEndpoint -> userInfoEndpoint
+                                                .userService(oAuth2UserService)
+                                )
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/member/logout")
+                );
         return http.build();
     }
     @Bean
